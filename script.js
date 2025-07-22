2b08f61eedb2fa88d5dc62299a82ff2be3be825b
satminpaing1 <big680415@gmail.com>
2025-07-22 09:39:37 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Firebase init example (make sure you add your real config here)
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
+
+window.addEventListener('load', () => {
+  console.log("App Loaded");
+});
